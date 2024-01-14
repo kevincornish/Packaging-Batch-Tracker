5d24270c2f49da29a1b501e16d55d60afe98787e
Kevin Cornish <kevinz_c@live.com>
2024-01-14 19:40:58 +0000

    db test

--- batchtracker/settings.py
+++ batchtracker/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'batchtracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'batchtracker',
+        'USER': 'kev',
+        'PASSWORD': 'zn5p88qe',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
