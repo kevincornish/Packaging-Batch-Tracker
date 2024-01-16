40efc07afa393ebb3ace2333fe259d07407f538c
Kevin Cornish <kevinz_c@live.com>
2024-01-16 21:23:38 +0000

    test

--- batchtracker/settings.py
+++ batchtracker/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Base url to serve media files
 MEDIA_URL = "/media/"
 
