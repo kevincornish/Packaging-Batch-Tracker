2ff523e6595745d912a7525328a0067c6066f6d0
Kevin Cornish <kevinz_c@live.com>
2024-01-16 21:39:07 +0000

    test

--- batchtracker/settings.py
+++ batchtracker/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/media/"
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
