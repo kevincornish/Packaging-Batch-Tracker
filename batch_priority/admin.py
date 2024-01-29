c81b118343f59bec982722f41426ea0c231ec5ea
Kevin Cornish <kevinz_c@live.com>
2024-01-29 20:10:41 +0000

    add comments to admin

--- batch_priority/admin.py
+++ batch_priority/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Batch, Product, TargetDate, Bay
+from .models import Batch, Product, TargetDate, Bay, Comment
 from simple_history.admin import SimpleHistoryAdmin
 
 admin.site.register(Batch, SimpleHistoryAdmin)
 admin.site.register(Product)
 admin.site.register(TargetDate, SimpleHistoryAdmin)
 admin.site.register(Bay)
+admin.site.register(Comment)
