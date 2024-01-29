96fc31beb119e6196c942be75337e5a5e8d65e1b
Kevin Cornish <kevinz_c@live.com>
2024-01-29 19:29:57 +0000

    only manager has access to add batches/bays/products

--- batch_priority/templatetags/custom_filters.py
+++ batch_priority/templatetags/custom_filters.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.filter
 def json_loads(value):
     return json.loads(value)
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
