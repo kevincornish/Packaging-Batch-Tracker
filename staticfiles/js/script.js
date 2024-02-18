c2be5b2f1eeec43b5a1d8945d26761e962655eda
Kevin Cornish <kevinz_c@live.com>
2024-02-18 13:44:55 +0000

    remove unused script

--- staticfiles/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var prodCheckElements = document.querySelectorAll(".prod-check-status");
-  prodCheckElements.forEach(function (elem) {
-    if (elem.textContent === "Error") {
-      elem.classList.add("text-danger");
-    }
-  });
-});
